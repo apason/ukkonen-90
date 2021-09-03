8a19864a33129ec60d1f07a2db4082dc8b786f72
apason <arttu.kilpinen@gmail.com>
2021-09-03 22:09:47 +0300

    measure_duplicates script modified to use percentage instead of a fixed input size

--- scripts/measure_duplicates.sh
+++ scripts/measure_duplicates.sh
@@ -7,7 +7,10 @@
 # This data set is not in the repo
 datadir="../data/pizzachili/real/"
 
-cuts=(50000 100000 200000 500000 1000000 2000000 5000000 10000000 20000000 40000000)
+# X cuts=(50000 100000 200000 500000 1000000 2000000 5000000 10000000 20000000 40000000)
+
+# comment tis out and uncomment X lines for size (not percentage) defined samples
+cuts=(0.0025 0.005 0.01 0.02 0.04 0.06 0.12)
 line_lengths=(128 256 512 1024 2048 4096 8192 16384)
 
 # Runtime of this script in haapa was 350 minutes.
@@ -25,11 +28,11 @@ for ll in ${line_lengths[@]} ; do
             for file in $(ls $datadir) ; do
 
 
-                input_size=$(wc -c "$datadir$file" | cut -d' ' -f 1)
+                # X input_size=$(wc -c "$datadir$file" | cut -d' ' -f 1)
 
-                cut=$(bc <<< "scale=10; $size/$input_size")
+                # X cut=$(bc <<< "scale=10; $size/$input_size")
 
-                echo -n "$file:$ll:$size:$cut:  " >> duplicate_results
+                echo -n "$file:$ll:$size:" >> duplicate_results
 
                 ../target/scs -b -l "$ll" -c "$cut" -f "$datadir$file" > output
 
@@ -42,3 +45,15 @@ for ll in ${line_lengths[@]} ; do
 
         done
 done
+
+
+## Use something like this to get the values in csv format
+#!/bin/bash
+#count=1
+#for line in $(cat duplicate_results | grep cere | sort -t ':' -k3 -n -s | cut -d':' -f 4 ) ; do
+#        if [ $(($count % 8)) -eq 0 ] ; then
+#                echo "$line" ;
+#        else  echo -n "$line," ;
+#        fi ;
+#        ((count++))
+#done
